546ac26cc3cf1d0060309d244906d36aa0912ba4
Patrick Fuller <patrickfuller@gmail.com>
2015-05-18 14:33:47 -0500

    Giving up on rST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="alicat",
-    version="0.1.10",
+    version="0.1.11",
     description="Python driver for Alicat mass flow controllers.",
     url="http://github.com/numat/alicat/",
     author="Patrick Fuller",
